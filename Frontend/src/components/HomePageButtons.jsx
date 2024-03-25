96bad03e9b7aa96a47d33a71922d6354a113959e
DarynaBb <darinabilenko@gmail.com>
2024-03-25 12:13:53 +0100

    apllying filter

--- Frontend/src/components/HomePageButtons.jsx
+++ Frontend/src/components/HomePageButtons.jsx
@@ -14,16 +14,16 @@ function HomePageButtons() {
     setIsCreateCardsClicked(true);
   };
   return (
-    <section className='flex gap-[24px] brightness-100'>
+    <section className='flex gap-[24px]'>
       <button
         onClick={() => navigate("/modules")}
-        className='bg-[#000] min-w-[140px] brightness-100 text-white whitespace-nowrap text-[1.2em] md:text-[1.6em] dm-sans-bold md:px-[48px] py-[17px] md:py-[24px] rounded-[36px] hover:bg-white hover:text-black'
+        className='bg-[#000] min-w-[140px] text-white whitespace-nowrap text-[1.2em] md:text-[1.6em] dm-sans-bold md:px-[48px] py-[17px] md:py-[24px] rounded-[36px] hover:bg-white hover:text-black'
       >
         STUDY SETS
       </button>
       <button
         onClick={onClickHandler}
-        className='bg-white min-w-[140px] brightness-100 whitespace-nowrap text-[1.2em] md:text-[1.6em] dm-sans-bold px-[25px] md:px-[36px] py-[9px] rounded-[36px] items-center flex gap-[6px] md:gap-[15px] lg:gap-[24px] justify-center hover:bg-[#FFC2FF]'
+        className='bg-white min-w-[140px] whitespace-nowrap text-[1.2em] md:text-[1.6em] dm-sans-bold px-[25px] md:px-[36px] py-[9px] rounded-[36px] items-center flex gap-[6px] md:gap-[15px] lg:gap-[24px] justify-center hover:bg-[#FFC2FF]'
       >
         <p>CREATE CARDS</p>
         <div className="bg-[#FFC2FF] p-[4px] md:p-[12px] rounded-full">

101ee4ac5d2a5adc18e6cbd9349879dbf5844e5d
DarynaBb <darinabilenko@gmail.com>
2024-03-26 14:59:47 +0100

    img size changed

--- Frontend/src/pages/Practice.jsx
+++ Frontend/src/pages/Practice.jsx
@@ -180,8 +180,8 @@ function Practice() {
                           <p className="text-[2.4em] md:text-[4em] text-center text-leading-[100%]">
                             {currentCard?.card?.question}
                           </p>
-                          <div className="min-w-[160px] md:max-w-[300px] min-h-[110px] md:max-h-[220px] flex justify-center mx-auto">
-                            <img className="w-full h-full object-cover" src={currentCard?.card?.image} alt="" />
+                          <div className="min-w-[160px] md:max-w-[300px] min-h-[110px] max-h-[220px] flex justify-center mx-auto">
+                            <img className="w-full h-full object-contain" src={currentCard?.card?.image} alt="" />
                           </div>
                           <p className="text-[1.4em] text-leading-[150%] self-center">
                             Show the answer
